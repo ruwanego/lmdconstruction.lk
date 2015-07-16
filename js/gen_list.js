b7401b8b264d68ad5502166501a23cd71376e059
Ruwan Egodawatte <ruwanego@gmail.com>
2015-07-16 22:17:16 +0530

    Reformatting Source Files

--- js/gen_list.js
+++ js/gen_list.js
@@ -1,50 +1,86 @@
 ---
----
+    ---
 
 
-function refreshFeatures() {
+        function refreshFeatures() {
 
 //  ------------------------------------------------ PPA Add scripts ------------------------------------------------///
-var PPA = "";
-{% capture posts %}
-{% for post in site.posts %}
+            var PPA = "";
+            {%
+                capture
+                posts %
+            }
+            {%
+                for post in site.posts %}
 
-if (document.getElementById("{{ post.name }}")) PPA += !document.getElementById("{{ post.name }}").checked ? '' : '{{ post.ppa }}\n' ;
+                if (document.getElementById("{{ post.name }}")) PPA += !document.getElementById("{{ post.name }}").checked ? '' : '{{ post.ppa }}\n';
 
-{% if forloop.last %}{% else %}{% endif %}
-{% endfor %}
+                {%
+                    if forloop.last %}
+                    {% else %
+                    }
+                    {%
+                        endif %
+                    }
+                    {%
+                        endfor %
+                    }
 
-{% endcapture %}
-{{posts | strip_newlines}}
+                    {%
+                        endcapture %
+                    }
+                    {
+                        {
+                            posts | strip_newlines
+                        }
+                    }
 
 
-PPA = PPA.substring(0, PPA.length - 1);
+                    PPA = PPA.substring(0, PPA.length - 1);
 
 //  ------------------------------------------------ // Install scripts //------------------------------------------------///
 
-var INSTALL = "";
+                    var INSTALL = "";
 
-{% capture posts %}
-{% for post in site.posts %}
+                    {%
+                        capture
+                        posts %
+                    }
+                    {%
+                        for post in site.posts %}
 
-if (document.getElementById("{{ post.name }}")) INSTALL += !document.getElementById("{{ post.name }}").checked ? '' : '{{ post.install }}\n' ;
+                        if (document.getElementById("{{ post.name }}")) INSTALL += !document.getElementById("{{ post.name }}").checked ? '' : '{{ post.install }}\n';
 
 
-{% if forloop.last %}{% else %}{% endif %}
-{% endfor %}
+                        {%
+                            if forloop.last %}
+                            {% else %
+                            }
+                            {%
+                                endif %
+                            }
+                            {%
+                                endfor %
+                            }
 
-{% endcapture %}
-{{posts | strip_newlines}}
-
+                            {%
+                                endcapture %
+                            }
+                            {
+                                {
+                                    posts | strip_newlines
+                                }
+                            }
 
 
 // Show Install scripts
-var packagelist = "";
+                            var packagelist = "";
 
-packagelist +="################################################################################" + "\n" +"######** Use this Package List with Cleanstart shell Script to install **#######"+ "\n" + "################################################################################" + "\n" + PPA + "\n" + INSTALL;
+                            packagelist += "################################################################################" + "\n" + "######** Use this Package List with Cleanstart shell Script to install **#######" + "\n" + "################################################################################" + "\n" + PPA + "\n" + INSTALL;
 
-$('#editor').html( packagelist );
+                            $('#editor').html(packagelist);
 
 
-};	
+                        }
+                        ;
 
